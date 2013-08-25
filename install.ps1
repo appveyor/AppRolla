00263bc1c304264af361e7107e2d187b511d8654
Feodor Fitsner <feodor@fitsner.com>
2013-08-25 09:37:49 -0700

    Added install.ps1 script

---
+++ install.ps1
@@ -0,0 +1,5 @@
+﻿$modules = "$home\Documents\WindowsPowerShell\Modules"
+Write-Host "Installing AppRolla module into your user profile: $modules"
+
+New-Item "$modules\AppRolla" -ItemType Directory -Force | Out-Null
+(New-Object Net.WebClient).DownloadFile("https://raw.github.com/AppVeyor/AppRolla/master/AppRolla.psm1", "$modules\AppRolla\AppRolla.psm1")
